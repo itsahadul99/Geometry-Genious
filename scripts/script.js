1512eed3b806a5a0f848171cb578e457f2970af7
itsahadul99 <itsahadul999@gmail.com>
2024-02-13 11:51:52 +0600

    complete rhombus area

--- scripts/script.js
+++ scripts/script.js
@@ -11,9 +11,9 @@ function triangleCalculateArea(){
     const area = 0.5 * base * height;
     const calculationContainer = document.getElementById('calculation-container');
     const p = document.createElement('p');
-    p.innerText =+ count + ('. ') + 'Triangle Area is: '+ area;
+    p.innerText =+ count + ('. ') + 'Rectangle Area is: '+ area;
     count++;
-    p.style.fontSize = '1.1rem';
+    p.style.fontSize = '.9rem';
     p.style.fontWeight = 'bold';
     calculationContainer.appendChild(p);
 }
@@ -28,12 +28,12 @@ function rectangleAreaCalculate(){
     const rectangleLengthText = rectangleLengthInput.value;
     const length = parseFloat(rectangleLengthText);
     // console.log(height);
-    const area = 0.5 * width * length;
+    const area = width * length;
     const calculationContainer = document.getElementById('calculation-container');
     const p = document.createElement('p');
     p.innerText =+ count + ('. ') + 'Rectangle Area is: '+ area;
     count++;
-    p.style.fontSize = '1.1rem';
+    p.style.fontSize = '.9rem';
     p.style.fontWeight = 'bold';
     calculationContainer.appendChild(p);
 }
@@ -49,12 +49,33 @@ function parallelogramAreaCalculate(){
     const parallelogramHeightText = parallelogramHeightInput.value;
     const height = parseFloat(parallelogramHeightText);
     // console.log(height);
-    const area = 0.5 * base * height;
+    const area = base * height;
     const calculationContainer = document.getElementById('calculation-container');
     const p = document.createElement('p');
     p.innerText =+ count + ('. ') + 'Parallelogram Area is: '+ area;
     count++;
-    p.style.fontSize = '1.1rem';
+    p.style.fontSize = '.9rem';
+    p.style.fontWeight = 'bold';
+    calculationContainer.appendChild(p);
+}
+
+// Rhombus part
+
+function rhombusAreaCalculate(){
+    const rhombusFirstD1Input = document.getElementById('rhombus-d1');
+    const rhombusFirstD1Text = rhombusFirstD1Input.value;
+    const d1 = parseFloat(rhombusFirstD1Text);
+    // console.log(base);
+    const rhombusFirstD2Input = document.getElementById('rhombus-d2');
+    const rhombusFirstD2Text = rhombusFirstD2Input.value;
+    const d2 = parseFloat(rhombusFirstD2Text);
+    // console.log(height);
+    const area = 0.5 * d1 * d2;
+    const calculationContainer = document.getElementById('calculation-container');
+    const p = document.createElement('p');
+    p.innerText =+ count + ('. ') + 'Rhombus Area is: '+ area;
+    count++;
+    p.style.fontSize = '.9rem';
     p.style.fontWeight = 'bold';
     calculationContainer.appendChild(p);
 }
