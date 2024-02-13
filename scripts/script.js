a0a9288e2f9679fd03b7daecf817ba2a86d8c58b
itsahadul99 <itsahadul999@gmail.com>
2024-02-13 11:27:17 +0600

    complete parallelogram area

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,44 @@ function triangleCalculateArea(){
     p.style.fontWeight = 'bold';
     calculationContainer.appendChild(p);
 }
+// Rectangle part 
+function rectangleAreaCalculate(){
+    
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    const width = parseFloat(rectangleWidthText);
+    // console.log(base);
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText);
+    // console.log(height);
+    const area = 0.5 * width * length;
+    const calculationContainer = document.getElementById('calculation-container');
+    const p = document.createElement('p');
+    p.innerText =+ count + ('. ') + 'Rectangle Area is: '+ area;
+    count++;
+    p.style.fontSize = '1.1rem';
+    p.style.fontWeight = 'bold';
+    calculationContainer.appendChild(p);
+}
+
+// Parallelogram part
+
+function parallelogramAreaCalculate(){
+    const parallelogramBaseInput = document.getElementById('parallelogram-base');
+    const parallelogramBaseText = parallelogramBaseInput.value;
+    const base = parseFloat(parallelogramBaseText);
+    // console.log(base);
+    const parallelogramHeightInput = document.getElementById('parallelogram-height');
+    const parallelogramHeightText = parallelogramHeightInput.value;
+    const height = parseFloat(parallelogramHeightText);
+    // console.log(height);
+    const area = 0.5 * base * height;
+    const calculationContainer = document.getElementById('calculation-container');
+    const p = document.createElement('p');
+    p.innerText =+ count + ('. ') + 'Parallelogram Area is: '+ area;
+    count++;
+    p.style.fontSize = '1.1rem';
+    p.style.fontWeight = 'bold';
+    calculationContainer.appendChild(p);
+}
